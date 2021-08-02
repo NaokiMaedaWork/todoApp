3163c41b27c7dd7e862b9aa78c714d4e28c661f7
前田尚輝 <>
2021-08-02 23:09:29 +0900

    update チェックボックスに変更

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -35,16 +35,15 @@ class TodoList extends React.Component {
         <ul className="todo-list">
           {this.state.items.map((item ,index) => {
             return (
-              <li className="todo-item" key={index}>
-                <button className={item.checked ? "todo-checked w3-button w3-green" : "todo-not-checked  w3-button w3-blue"} onClick={() => this.changeItemStatus(index)}>
-                  {item.checked ? "✅" : "⭕️"}
-                </button>
-                <input type="text" name="overview" className="w3-input w3-padding-16 w3-border" onChange={this.changeOverView}></input>
+              <li className="todo-item w3-row w3-padding-16" key={index}>
+                  <input type="checkbox" name="todo-status" className="todo-status-button w3-col" onChange={() => this.changeItemStatus(index)}>
+                  </input>
+                  <input type="text" name="overview" className="todo-over-view w3-input w3-col w3-padding-16 w3-border" onChange={this.changeOverView}></input>
               </li>
             )
           })}
         </ul>
-        <div class="todo-buttons">
+        <div className="todo-buttons">
           <button id="todo-add-item" className="w3-button w3-black" onClick={this.addItem}>
             ADD NEW ITEM
           </button>
