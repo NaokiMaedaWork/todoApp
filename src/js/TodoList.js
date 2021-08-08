41345cd6e3dc54894a4cc9400beee965c94e9563
前田尚輝 <>
2021-08-08 23:16:12 +0900

    update 削除アイコン追加、削除ボタン押下時にチェックボックスにチェックが入っている項目の削除処理追加

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -19,29 +19,46 @@ class TodoList extends React.Component {
     // add new item
     const currentItems = this.state.items;
     currentItems.push({overview ,date ,checked});
-    this.setState({items: currentItems})
+    this.setState({items: currentItems});
   }
   changeItemStatus = (index) =>{
-    const currentItems = [...this.state.items];
+    let currentItems = [...this.state.items];
     currentItems[index].checked = !currentItems[index].checked;
-    this.setState({items: currentItems})
+    this.setState({items: currentItems});
   }
-  changeOverView = () =>{
+  removeItem = () => {
+    let currentItems = [...this.state.items];
 
+    let i;
+    let removeTargetArray = [];
+    for(i = 0; i<=currentItems.length - 1; i++){
+      if(currentItems[i].checked){
+        removeTargetArray.push(i);
+      }
+    }
+    for(i = removeTargetArray.length; i--;){
+      currentItems.splice(removeTargetArray[i],1);
+    }
+    this.setState({items: currentItems});
   }
+
   render(){
+    const todoItems = this.state.items.map((item ,index) => {
+      return (
+        <li className="todo-item w3-row w3-padding-16" key={index}>
+            <input type="checkbox" className="todo-status-button w3-col" checked={item.checked} onChange={() => this.changeItemStatus(index)}></input>
+            <input type="text" name="overview" className="todo-over-view w3-input w3-col w3-border"></input>
+        </li>
+      )
+    })
     return (
       <div className="w3-container w3-center">
-        <ul className="todo-list">
-          {this.state.items.map((item ,index) => {
-            return (
-              <li className="todo-item w3-row w3-padding-16" key={index}>
-                  <input type="checkbox" name="todo-status" className="todo-status-button w3-col" onChange={() => this.changeItemStatus(index)}>
-                  </input>
-                  <input type="text" name="overview" className="todo-over-view w3-input w3-col w3-padding-16 w3-border" onChange={this.changeOverView}></input>
-              </li>
-            )
-          })}
+        <div>
+          <button id="todo-remove-item-image" className="w3-right w3-button todo-remove-item" onClick={this.removeItem}>
+          </button>
+        </div>
+        <ul className="todo-list w3-table">
+          {todoItems}
         </ul>
         <div className="todo-buttons">
           <button id="todo-add-item" className="w3-button w3-black" onClick={this.addItem}>
